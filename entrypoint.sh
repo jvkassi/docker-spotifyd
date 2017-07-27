93cde3e6458692609fd099fb607a078c7aedf0d1
Jean-Vincent KASSI <jekas@smile.ci>
2017-07-27 16:30:45 +0000

    add device variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,10 @@ then
    exit 0
 fi
 
-spotifyd -u $USERNAME -p $PASSWORD --device_name $USERNAME
+DEVICE=${DEVICE:-$(echo $USERNAME | cut -d @ -f 1 | sed 's/\./-/' | sed 's/_//')}
+
+# Launch spotifyd in background
+spotifyd -u $USERNAME -p $PASSWORD --device_name $DEVICE
 
 exec spotifyd-http -u $USERNAME -p $PASSWORD -n `hostname`
 
